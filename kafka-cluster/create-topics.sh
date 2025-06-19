55a437f3d524b2a8283fe940a486e9fecc71ca18
Rajiv Kumar <ecominds7@gmail.com>
2025-06-19 23:37:19 +0530

    test2

--- kafka-cluster/create-topics.sh
+++ kafka-cluster/create-topics.sh
@@ -3,8 +3,20 @@
 #echo "Waiting for Kafka to be ready..."
 #sleep 10  # Allow time for Kafka to fully start
 
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic ecominds-topic --partitions 3 --replication-factor 1
+cat <<EOF > /tmp/client.properties
+security.protocol=SASL_PLAINTEXT
+sasl.mechanism=PLAIN
+sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username="kafkaclient" password="kafkasecret";
+EOF
 
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic test-topic --partitions 1 --replication-factor 1
+
+kafka-topics.sh --bootstrap-server localhost:9092 --command-config /tmp/client.properties --list
+
+
+kafka-topics.sh --bootstrap-server localhost:9092 --command-config /tmp/client.properties --list
+
+kafka-topics.sh --bootstrap-server localhost:9092 --command-config /tmp/client.properties --create --if-not-exists --topic ecominds-topic --partitions 3 --replication-factor 1
+
+kafka-topics.sh --bootstrap-server localhost:9092 --command-config /tmp/client.properties --create --if-not-exists --topic test-topic --partitions 1 --replication-factor 1
 
 echo "2 Topics are created"
